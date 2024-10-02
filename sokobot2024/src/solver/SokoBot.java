00de79385aff785a16739eea2cbb7d99f0711205
Ellexandrei Esponilla <xandrei37@gmail.com>
2024-10-02 21:37:18 +0800

    created function to get the coordinate of the plyr

--- sokobot2024/src/solver/SokoBot.java
+++ sokobot2024/src/solver/SokoBot.java
@@ -11,6 +11,8 @@ public class SokoBot {
      * sequence
      * that just moves left and right repeatedly. test5
      */
+    int playerCoord[] = getPlayerCoord(itemsData);
+    System.out.println(playerCoord[0] + ", " + playerCoord[1]);
 
     try {
       Thread.sleep(3000);
@@ -18,6 +20,29 @@ public class SokoBot {
       ex.printStackTrace();
     }
     return "lrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlr";
+  }
+
+  /**
+   * 
+   * @param itemsData
+   * @return the player coordinate in an array, the array will contain -1, -1 if
+   *         it did not find the player
+   */
+  public int[] getPlayerCoord(char[][] itemsData) {
+    int playerCoord[] = new int[2];
+    for (int i = 0; i < itemsData.length; i++) {
+      for (int j = 0; j < itemsData[i].length; j++) {
+        if (itemsData[i][j] == '@') {
+          playerCoord[0] = i;
+          playerCoord[1] = j;
+          return playerCoord;
+        }
+      }
+    }
+
+    playerCoord[0] = -1;
+    playerCoord[1] = -1;
+    return playerCoord;
   }
 
 }
